db627fb43fa67bf3f301d86ffd20dbf9d9f0134a
Geethufama <geethufama@gmail.com>
2026-02-02 17:24:05 +0400

    spelling change

--- app/Models/agreementSubunitRentBifurcation.php
+++ app/Models/agreementSubunitRentBifurcation.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class agreementSubunitRentBifurcation extends Model
+class AgreementSubunitRentBifurcation extends Model
 {
     use HasFactory, SoftDeletes, HasActivityLog, HasDeletedBy;
 
