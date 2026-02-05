f09bf4da0336ac7520cfe2c3d69b0d247bffa5b6
Geethufama <geethufama@gmail.com>
2026-02-05 14:43:09 +0400

    delete console error fix

--- resources/views/admin/investment/investment/investment.blade.php
+++ resources/views/admin/investment/investment/investment.blade.php
@@ -522,9 +522,10 @@
                         }
                     });
 
-                } else {
-                    toastr.error(errors.responseJSON.message);
                 }
+                //  else {
+                //     toastr.error(errors.responseJSON.message);
+                // }
             });
         }
         $(document).on('click', '.openTerminationModal', function() {
