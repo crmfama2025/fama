3183a3d4ad7fc5e347a830af253f43ae69e3eda2
Geethufama <geethufama@gmail.com>
2026-04-14 11:23:41 +0400

    email check

--- app/Services/Contracts/ContractCommentService.php
+++ app/Services/Contracts/ContractCommentService.php
@@ -48,7 +48,7 @@ class ContractCommentService
             if ($contract->contract_status == 4) {
                 $result = $this->brevoService->sendEmail(
                     [
-                        ['email' => 'geethufama@gmail.com', 'name' => 'Test User']
+                        ['email' => 'rahmathrasmiya@gmail.com', 'name' => 'Test User']
                     ],
                     'Kindly Review and Approve Contract',
                     'admin.emails.contract-approval-email',
