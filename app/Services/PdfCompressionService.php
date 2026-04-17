65bfacf671f4cd66756547ab81a39cf30f3d6449
Geethufama <geethufama@gmail.com>
2026-04-17 12:24:50 +0400

    test

--- app/Services/PdfCompressionService.php
+++ app/Services/PdfCompressionService.php
@@ -6,46 +6,7 @@ use App\Jobs\CompressPdfJob;
 
 class PdfCompressionService
 {
-    // public function compress($file, $path, $filename)
-    // {
-    //     // $extension = pathinfo($filename, PATHINFO_EXTENSION);
-    //     // $baseName  = pathinfo($filename, PATHINFO_FILENAME);
-    //     // $baseName  = preg_replace('/[^A-Za-z0-9._\-]/', '_', $baseName);
-    //     // $filename  = $baseName . '.' . $extension;
-    //     // Temp upload
-    //     $tempPath = $file->storeAs('temp', $filename, 'public');
 
-    //     $inputPath = storage_path('app/public/' . $tempPath);
-    //     $finalDir = storage_path('app/public/' . $path);
-    //     $finalPath = $finalDir . '/' . $filename;
-
-    //     if (!file_exists($finalDir)) {
-    //         mkdir($finalDir, 0777, true);
-    //     }
-
-    //     $gsPath = PHP_OS_FAMILY === 'Windows' ? 'gswin64c' : 'gs';
-
-    //     $command = $gsPath . ' -sDEVICE=pdfwrite '
-    //         . '-dCompatibilityLevel=1.4 '
-    //         . '-dPDFSETTINGS=/ebook '
-    //         . '-dNOPAUSE -dQUIET -dBATCH '
-    //         . '-sOutputFile=' . escapeshellarg($finalPath) . ' '
-    //         . escapeshellarg($inputPath);
-
-
-    //     // // dd($inputPath, $finalPath, $command);
-    //     // $originalSize   = filesize($inputPath);
-    //     // $compressedSize = filesize($finalPath);
-    //     // dd($originalSize, $compressedSize);
-
-    //     // exec($command);
-    //     exec($command, $output, $returnCode);
-    //     // dd($command, $output, $returnCode);
-
-    //     unlink($inputPath);
-
-    //     return $path . '/' . $filename;
-    // }
     public function compress($file, $path, $filename): string
     {
         dd("test");
