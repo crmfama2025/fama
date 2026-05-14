e8c896aeec05493f65bfd02d7881d87b32710999
Geethufama <geethufama@gmail.com>
2026-05-14 12:21:48 +0400

    contract details table duration in months change

---
+++ database/migrations/2026_05_14_121806_modify_contract_details_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::table('contract_details', function (Blueprint $table) {
+            $table->decimal('duration_in_months', 5, 2)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::table('contract_details', function (Blueprint $table) {
+            $table->integer('duration_in_months')->change();
+        });
+    }
+};
