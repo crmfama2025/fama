8c2de076b662e1116dba8036e5b56f8894e87b8b
Geethufama <geethufama@gmail.com>
2026-08-12 15:27:48 +0400

    update

--- app/Services/Investment/InvestmentContractService.php
+++ app/Services/Investment/InvestmentContractService.php
@@ -204,7 +204,7 @@ class InvestmentContractService
 
             // Grand totals
             '{invested_amount}' => number_format($grandTotalInvested, 2),
-            '{invested_amount_eng}' => numberToEnglishWords($grandTotalInvested) . 'Dirhams Only',
+            '{invested_amount_eng}' => numberToEnglishWords($grandTotalInvested) . ' Dirhams Only',
             '{invested_amount_ar}' => numberToArabicWords($grandTotalInvested) . ' فقط',
             '{total_invested_amount}' => number_format($grandTotalInvested, 2),
             '{total_profit}'          => number_format($grandTotalProfit, 2),
@@ -759,7 +759,7 @@ class InvestmentContractService
             '{investment_date_eng}'      => date('d M Y', strtotime($investmentData->investment_date)),
             '{investment_date_ar}'       => arabicShortDate($investmentData->investment_date),
             '{invested_amount}'          => $investmentData->investment_amount,
-            '{invested_amount_eng}' => numberToEnglishWords($investmentData->investment_amount) . 'Dirhams Only',
+            '{invested_amount_eng}' => numberToEnglishWords($investmentData->investment_amount) . ' Dirhams Only',
             '{invested_amount_ar}' => numberToArabicWords($investmentData->investment_amount) . ' فقط',
             '{grace_period}'             => $investmentData->grace_period,
             '{mode_of_payment_eng}'      => $investorData->paymentMode->payment_mode_name,
