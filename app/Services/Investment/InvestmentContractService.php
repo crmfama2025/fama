d70c94efbcdc61a88abc3b7bf58e37b144983fc3
Geethufama <geethufama@gmail.com>
2026-08-12 10:44:15 +0400

    investment amount

--- app/Services/Investment/InvestmentContractService.php
+++ app/Services/Investment/InvestmentContractService.php
@@ -759,7 +759,7 @@ class InvestmentContractService
             '{investment_date_eng}'      => date('d M Y', strtotime($investmentData->investment_date)),
             '{investment_date_ar}'       => arabicShortDate($investmentData->investment_date),
             '{invested_amount}'          => $investmentData->investment_amount,
-            '{invested_amount_eng}' => numberToEnglishWords($investmentData->investment_amount) . ' Only',
+            '{invested_amount_eng}' => numberToEnglishWords($investmentData->investment_amount) . 'Dirhams Only',
             '{invested_amount_ar}' => numberToArabicWords($investmentData->investment_amount) . ' فقط',
             '{grace_period}'             => $investmentData->grace_period,
             '{mode_of_payment_eng}'      => $investorData->paymentMode->payment_mode_name,
