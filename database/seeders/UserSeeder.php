25496ba459a420ffc53eff759cb328da779bd337
Geethufama <geethufama@gmail.com>
2026-08-26 17:42:27 +0400

    superadmin mail id change in user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -28,7 +28,7 @@ class UserSeeder extends Seeder
         }
 
         $user_id = User::updateOrCreate(
-            ['email' => 'superadmin@demo.com'],
+            ['email' => 'crmfama2025@demo.com'],
             [
                 'user_code' => 'USR0001',
                 'first_name' => 'Super',
