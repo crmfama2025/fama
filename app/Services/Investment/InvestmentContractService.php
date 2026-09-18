e2ccb770f1fd29e5b5b80d3a9704433faad8cb08
Geethufama <geethufama@gmail.com>
2026-09-18 14:13:59 +0400

    update

--- app/Services/Investment/InvestmentContractService.php
+++ app/Services/Investment/InvestmentContractService.php
@@ -218,7 +218,7 @@ class InvestmentContractService
                 $investorSignText_ar = "<p class='marginClass text-sm'>(الوصي) المست: {$investorData->investorGuardian->guardian_name_arabic}</p>";
             } else {
                 $investorSignText = "<p class='marginClass text-sm'>Investor :{$investorData->investor_name}</p>";
-                $investorSignText_ar = "<p class='marginClass text-sm'>{$investorData->investor_name_arabic}:الوصي المست</p>";
+                $investorSignText_ar = "<p class='marginClass text-sm'>المستثمر:{$investorData->investor_name_arabic} </p>";
             }
         }
 
